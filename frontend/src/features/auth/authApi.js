39a5a24134c2df5a291e6c188ce541c94b5ffcba
Carmine Choi <carmine.choi@gmail.com>
2023-04-19 15:22:50 -0700

    fix: logout error

--- frontend/src/features/auth/authApi.js
+++ frontend/src/features/auth/authApi.js
@@ -4,57 +4,56 @@ import { setAuthDetails, removeAuthDetails } from "./authSlice";
 const BASE_URL = process.env.REACT_APP_SERVER_ENDPOINT;
 
 export const authApi = createApi({
-    reducerPath: "authApi",
-    baseQuery: fetchBaseQuery({
-        baseUrl: `${BASE_URL}/api/auth`,
-        credentials: "include",
-    }),
-    endpoints: (builder) => ({
-        registerUser: builder.mutation({
-            query: (user) => ({
-                url: "/register",
-                method: "POST",
-                body: user,
-            }),
-            async onQueryStarted(args, { dispatch, queryFulfilled }) {
-                try {
-                    const { data } = await queryFulfilled;
-                    dispatch(setAuthDetails(data));
-                } catch (e) {}
-            },
-        }),
-        loginUser: builder.mutation({
-            query: (user) => ({
-                url: "/login",
-                method: "POST",
-                body: user,
-            }),
-            async onQueryStarted(args, { dispatch, queryFulfilled }) {
-                try {
-                    const { data } = await queryFulfilled;
-                    dispatch(setAuthDetails(data));
-                } catch (e) {}
-            },
-        }),
-        logoutUser: builder.mutation({
-            query: () => ({
-                url: "/logout",
-                method: "DELETE",
-            }),
-            async onQueryStarted(args, { dispatch, queryFulfilled }) {
-                try {
-                    await queryFulfilled;
-                    dispatch(removeAuthDetails());
-                    dispatch(removeUserDetails());
-                } catch (e) {}
-            },
-        }),
-    }),
+	reducerPath: "authApi",
+	baseQuery: fetchBaseQuery({
+		baseUrl: `${BASE_URL}/api/auth`,
+		credentials: "include",
+	}),
+	endpoints: (builder) => ({
+		registerUser: builder.mutation({
+			query: (user) => ({
+				url: "/register",
+				method: "POST",
+				body: user,
+			}),
+			async onQueryStarted(args, { dispatch, queryFulfilled }) {
+				try {
+					const { data } = await queryFulfilled;
+					dispatch(setAuthDetails(data));
+				} catch (e) {}
+			},
+		}),
+		loginUser: builder.mutation({
+			query: (user) => ({
+				url: "/login",
+				method: "POST",
+				body: user,
+			}),
+			async onQueryStarted(args, { dispatch, queryFulfilled }) {
+				try {
+					const { data } = await queryFulfilled;
+					dispatch(setAuthDetails(data));
+				} catch (e) {}
+			},
+		}),
+		logoutUser: builder.mutation({
+			query: () => ({
+				url: "/logout",
+				method: "DELETE",
+			}),
+			async onQueryStarted(args, { dispatch, queryFulfilled }) {
+				try {
+					dispatch(removeAuthDetails());
+					dispatch(removeUserDetails());
+				} catch (e) {}
+			},
+		}),
+	}),
 });
 
 export const {
-    useRegisterUserMutation,
-    useLoginUserMutation,
-    useLogoutUserMutation,
-    useGetAccessTokenMutation,
+	useRegisterUserMutation,
+	useLoginUserMutation,
+	useLogoutUserMutation,
+	useGetAccessTokenMutation,
 } = authApi;
