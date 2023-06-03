80da740aca7a6a1aee5a23cbf95b8a8ec0863722
Carmine <carmine.choi@gmail.com>
2023-06-02 18:48:12 -0700

    fix: search text placeholder is flexible

--- frontend/src/components/SearchTextField.js
+++ frontend/src/components/SearchTextField.js
@@ -1,24 +1,22 @@
 import React from "react";
-import { Box, TextField, InputAdornment } from "@mui/material";
+import { TextField, InputAdornment } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 
-function SearchTextField() {
-    return (
-        <Box sx={{ display: "flex", alignItems: "flex-end" }}>
-            <TextField
-                fullWidth
-                type="search"
-                placeholder="Search for your integration"
-                InputProps={{
-                    startAdornment: (
-                        <InputAdornment position="start">
-                            <SearchIcon />
-                        </InputAdornment>
-                    ),
-                }}
-            />
-        </Box>
-    );
+function SearchTextField({ placeholder }) {
+	return (
+		<TextField
+			fullWidth
+			type="search"
+			placeholder={placeholder}
+			InputProps={{
+				startAdornment: (
+					<InputAdornment position="start">
+						<SearchIcon />
+					</InputAdornment>
+				),
+			}}
+		/>
+	);
 }
 
 export default SearchTextField;
