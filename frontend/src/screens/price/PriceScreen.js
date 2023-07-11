0d0f24cb58ae2783724164711037abe59a19b52c
Carmine Choi <carmine.choi@gmail.com>
2023-07-11 14:50:31 -0700

    fix: remove tab group from price table

--- frontend/src/screens/price/PriceScreen.js
+++ frontend/src/screens/price/PriceScreen.js
@@ -2,14 +2,11 @@ import React, { useEffect } from "react";
 import { Box, Container, Typography, Alert } from "@mui/material";
 import AppBar from "../../components/AppBar";
 import PriceTable from "./components/PriceTable";
-import TabsGroup from "../../components/tabs/TabsGroup";
 import CircularProgress from "../../components/CircularProgress";
 
 import { useGetAllCoinsMutation } from "../../features/coin/coinApi";
 import Footer from "../../components/Footer";
 import withRoot from "../../theme/withRoot";
-
-const priceTableTabs = ["All", "Portfolio", "Watchlist"];
 
 function PriceScreen() {
 	const [getAllCoins, { isError, isSuccess }] = useGetAllCoinsMutation();
@@ -31,9 +28,6 @@ function PriceScreen() {
 				<Typography variant="h6" sx={{ py: 2 }}>
 					Top 100 cryptocurrency prices, live charts, and market caps
 				</Typography>
-			</Container>
-
-			<TabsGroup tabs={priceTableTabs}>
 				{isError ? (
 					<Alert severity="error">Failed to fetch table data</Alert>
 				) : isSuccess ? (
@@ -41,7 +35,8 @@ function PriceScreen() {
 				) : (
 					<CircularProgress />
 				)}
-			</TabsGroup>
+			</Container>
+
 			<Footer />
 		</Box>
 	);
