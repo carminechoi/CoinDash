e950d2aa03ab0cb951b9214ca6946b2d2bd6158a
Carmine Choi <carmine.choi@gmail.com>
2023-09-13 13:45:27 -0700

    fix: news api no longer supports cointelegraph

--- backend/src/services/news.service.js
+++ backend/src/services/news.service.js
@@ -1,7 +1,8 @@
 const fetchCryptoNews = async () => {
 	const newsDataURL = process.env.NEWSDATA_URL;
 	const newsDataApiKey = process.env.NEWSDATA_KEY;
-	const apiUrl = `${newsDataURL}?apikey=${newsDataApiKey}&q=crypto&language=en&domain=cointelegraph`;
+	const apiUrl = `${newsDataURL}?apikey=${newsDataApiKey}&q=crypto&language=en&domain=bitcoinsistemi`;
+
 	try {
 		const response = await fetch(apiUrl);
 		const responseJson = await response.json();
